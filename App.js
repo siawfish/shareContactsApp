d78af8b4745ff4ace103a34d21fc5f30a340ce32
Gerald <mcamanor@gmail.com>
2020-05-16 14:59:38 +0000

    Completed signin screen design

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Splash from './src/screens/Splash';
 import Intro from './src/screens/Intro';
+import Signin from './src/screens/Signin';
 
 const Stack = createStackNavigator()
 
@@ -19,15 +20,29 @@ export default function App() {
           name='Splash' 
           component={Splash}
         /> */}
-        <Stack.Screen
+        {/* <Stack.Screen
           options={{
             headerShown:false
           }} 
           name='Welcome'
           component={Intro}
+        /> */}
+        <Stack.Screen
+          options={{
+            headerTitleStyle: {
+              fontWeight:'100',
+              fontSize:30,
+            },
+            headerStyle: {
+              backgroundColor:'#cf53b2',
+              height:100
+            },
+            headerTintColor:'#fff'
+          }} 
+          name='Sign In'
+          component={Signin}
         />
         {/* <Stack.Screen/>
-        <Stack.Screen/>
         <Stack.Screen/> */}
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/Signin.js
+++ src/screens/Signin.js
@@ -1,12 +1,100 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet, Image, Text, TextInput, TouchableOpacity } from 'react-native'
 
 export default class Signin extends Component {
     render() {
         return (
-            <View>
-                
+            <View style={styles.container}>
+                <View style={styles.imgContainer}>
+                    <Image style={styles.img} source={require('../../assets/signin.png')}/>
+                </View>
+                <View style={styles.formContainer}>
+                    <View style={styles.inputContainer}>
+                        <Text>Email</Text>
+                        <TextInput placeholder='xyz@codetrain.com' placeholderTextColor='#aaa'/>
+                    </View>
+                    <View style={styles.inputNBContainer}>
+                        <Text>Password</Text>
+                        <TextInput placeholder='Password' placeholderTextColor='#aaa' secureTextEntry={true}/>
+                    </View>
+                    <TouchableOpacity style={styles.btn}>
+                        <Text style={styles.btnText}>SIGN IN</Text>
+                    </TouchableOpacity>
+                    <View style={styles.resetContainer}>
+                        <Text>Forgot Password?</Text>
+                        <TouchableOpacity style={styles.underline}>
+                            <Text style={styles.reset}>Reset Password</Text>
+                        </TouchableOpacity>
+                        
+                    </View>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor:'#fff'
+    },
+
+    imgContainer: {
+        width:'100%',
+        height:250,
+    },
+
+    img: {
+        width:'100%',
+        height:250,
+        resizeMode:'center'
+    },
+
+    formContainer: {
+        marginVertical:50,
+        marginHorizontal:50
+    },
+
+    inputContainer: {
+        flexDirection:'row',
+        justifyContent:'space-between',
+        borderBottomWidth:1,
+        borderBottomColor:'#d0d0d0',
+        paddingVertical:15
+    },
+
+    inputNBContainer: {
+        flexDirection:'row',
+        justifyContent:'space-between',
+        paddingVertical:15,
+    },
+
+    btn:{
+        paddingVertical:20,
+        alignItems:'center',
+        backgroundColor: '#cf53b2',
+        marginVertical:20,
+        borderRadius:4
+    },
+
+    btnText: {
+        color:'#fff',
+        letterSpacing:3,
+        fontWeight:'400'
+    },
+
+    resetContainer: {
+        flexDirection:'row',
+        marginVertical:30,
+    },
+
+    underline: {
+        borderBottomColor:'#cf53b2',
+        borderBottomWidth:1,
+        marginStart:10
+    },
+    
+    reset: {
+        paddingBottom:3
+    }
+})
