796bf2871aef0fc32c5324c72be24d365854ca32
Gerald <mcamanor@gmail.com>
2020-05-16 10:51:26 +0000

    Completed splash screen design

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, Image } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default class Splash extends Component {
@@ -9,13 +9,15 @@ export default class Splash extends Component {
                 <View style={styles.logoContainer}>
                     <Image style={styles.logo} source={require('../../assets/logo.png')}/>
                 </View>
-                <View style={styles.descContainer}>
-                    <Text style={styles.desc}>CODETRAIN CONTACTS SHARING APP PROJECT</Text>
-                    <Text style={styles.small}>By @wolfiesiaw</Text>
+                <View style={styles.betterAlign}>
+                    <View style={styles.descContainer}>
+                        <Text style={styles.desc}>CODETRAIN CONTACTS SHARING APP PROJECT</Text>
+                        <Text style={styles.small}>By @wolfiesiaw</Text>
+                    </View>
+                    <TouchableOpacity style={styles.getStartedContainer}>
+                        <Text style={styles.getStarted} textDecorationStyle='solid' textDecorationColor='red'>GET STARTED</Text>
+                    </TouchableOpacity>
                 </View>
-                <TouchableOpacity >
-                    <Text>GET STARTED</Text>
-                </TouchableOpacity>
             </View>
         )
     }
@@ -25,9 +27,47 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'space-between',
+      alignItems:'center',
     },
 
+    logoContainer: {
+        marginTop:150,
+    },
 
+    logo: {
+        width:300,
+        height:60,
+        resizeMode:'center'
+    },
+
+    betterAlign: {
+        flex:1,
+        justifyContent:'space-between',
+        marginVertical:150,
+        alignItems:'center',
+    },
+
+    descContainer: {
+        alignItems:'center',
+        width:300
+    },
+
+    desc: {
+        fontSize:25,
+        textAlign:'center',
+        marginVertical:20,
+        fontWeight:'300',
+        lineHeight:45,
+        letterSpacing:3
+    },
+
+    small: {
+        fontWeight:'100'
+    },
+
+    getStartedContainer: {
+        borderBottomWidth: 3,
+        borderBottomColor: '#cf53b2',
+        padding:5
+    }
 });
