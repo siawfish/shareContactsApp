eba5d0ff507047c08081835e2e25c8a88c947763
Gerald <mcamanor@gmail.com>
2020-06-21 03:34:39 +0000

    added redux firestore

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
 import authReducer from './reducers/authReducer'
 import thunk from 'redux-thunk'
+import { reduxFirestore, getFirestore} from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
 
-let store = createStore(authReducer, applyMiddleware(thunk))
+let store = createStore(authReducer, 
+        compose(applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+        reduxFirestore(firebase),
+        reactReduxFirebase(firebase, { attachAuthIsReady: true})
+        )
+    )
 
 export default store
